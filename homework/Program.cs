791de29a91a88096a8e5ff76802e0d0d24fe3b25
ismayilhasan <ismayilrha@code.edu.az>
2022-04-13 18:57:10 +0400

    task(Homework)

---
+++ homework/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eded =");
+            int numb = Convert.ToInt32(Console.ReadLine());    
+
+            if(numb % 2 == 0)
+            {
+                Console.WriteLine(numb + " " + "cut ededdir");
+            }
+            else
+            {
+                Console.WriteLine(numb + " " + " tek ededdir");
+
+            }
+
+
+        }
+    }
+}
