0f26f9f184ff483d9d08c8b852094e725d3a61bd
ismayilhasan <102584457+ismayilhasan@users.noreply.github.com>
2022-04-13 19:01:48 +0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace homework_boyuk_kicik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            Console.WriteLine("A = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("B = ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            if (a > b)
+            {
+                Console.WriteLine("A ededi B den boyukdur (A > B)");
+            }
+            else if (b>a)
+            {
+                Console.WriteLine("B ededi a dan boyukdur (B > A)");
+            }
+            else
+            {
+                Console.WriteLine("ededler berabedir");
+            }
+
+        }
+    }
+}
