b72a824a81543dcc1f3da260769dbd536212ab19
ismayilhasan <ismayilrha@code.edu.az>
2022-04-14 13:19:21 +0400

    homework task 1

---
+++ homework_task3/homework_task/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0,i = 0;
+         
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            while(num > 0)
+            {
+                i = num % 10;
+                sum += i;
+                num /= 10;
+            }
+
+            Console.WriteLine(sum);
+
+
+
+
+
+        }
+    }
+}
