c51be80cedb250859a452a89fc3b9ecc09cdc4d1
ismayilhasan <ismayilrha@code.edu.az>
2022-04-14 16:15:53 +0400

    array task

---
+++ homework_task_array/homework_task_array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace homework_task_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "Seymur","Kerim","AZE", "Nihad","Azerbaycan","Respublikasi","WWW" };
+
+            
+            int count = default;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i].Length > 5)
+                {
+                    Console.WriteLine(array[i]);
+
+                }
+
+            }
+
+
+
+        }
+    }
+}
