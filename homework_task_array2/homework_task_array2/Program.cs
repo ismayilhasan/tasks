0076faeede582f1bfae1f6ae37dadb17abab301d
ismayilhasan <ismayilrha@code.edu.az>
2022-04-18 17:37:56 +0400

    task3 `18.04.2022

---
+++ homework_task_array2/homework_task_array2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace homework_task_array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number :: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[default];
+            for(int i = 0; i<=number; i++)
+            {
+                 
+                if(number < 50)
+                {
+                    
+                    if (i % 3 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+
+
+
+                else if (number < 100 && number > 50)
+                {
+
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                }
+
+                else if (number > 100)
+                {
+                    if(i % 8 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+
+            }
+        }
+    }
+}
