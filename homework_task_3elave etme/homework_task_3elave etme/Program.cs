ba6db1a386b552899f7b375348e176ffc106b74f
ismayilhasan <ismayilrha@code.edu.az>
2022-04-18 17:35:46 +0400

    task2 18.04.2022

---
+++ homework_task_3elave etme/homework_task_3elave etme/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace homework_task_3elave_etme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write( "NUMBER :: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+;                add(number);
+
+        }
+
+
+
+
+        public static void add(int num)
+        {
+            Console.Write("Number : ");
+            
+
+            int count = num; int i = 3;
+
+            while (count > 0)
+            {
+                i *= 10;
+                count /= 10;
+
+            }
+
+            Console.WriteLine("new number :: " + (num + i));
+
+            
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
