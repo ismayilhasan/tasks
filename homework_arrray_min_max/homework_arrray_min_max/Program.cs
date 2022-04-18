834ba408f1d94aaa826232f4b511fadb7347b147
ismayilhasan <ismayilrha@code.edu.az>
2022-04-19 03:41:03 +0400

    Homework task 19.04.2022

---
+++ homework_arrray_min_max/homework_arrray_min_max/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace homework_arrray_min_max
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Massivin Uzunlugu  ::");
+            int size =  Convert.ToInt32( Console.ReadLine());  
+            int[] array = new int[size];
+            int number = default;
+
+            int largest = 0;
+            for(int i = 0;i<size;i++)
+            {
+                Console.WriteLine(i + 1 + "ci eded ::");
+
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            
+
+            foreach (int item in array)
+            {
+                if (largest < item)
+                {
+                    largest = item;
+                }
+            }
+       
+
+            Console.WriteLine(largest + "is max in array");
+
+            
+
+        }
+    }
+}
