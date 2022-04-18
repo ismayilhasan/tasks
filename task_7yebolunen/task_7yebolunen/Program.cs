063020d98cfc69e24c0ddfb835b35ebd98f7ef26
ismayilhasan <ismayilrha@code.edu.az>
2022-04-18 17:32:33 +0400

    Homework task 18.04.2022

---
+++ task_7yebolunen/task_7yebolunen/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace task_7yebolunen
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number :: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int count_1 = 1;
+            int count_2 = 1;
+            if (number % 7 == 0)
+            {
+                Console.WriteLine(number + "ededi 7 ye bolunur");
+            }
+
+            else
+            {
+                int new_number = number;
+
+                while (count_1 <= 3)
+                {
+
+                    number++;
+                    count_1 += 1;
+                    if (number % 7 == 0)
+                    {
+                        Console.WriteLine(number);
+                    }
+
+
+                }
+                while (count_2 <= 3)
+                {
+                    new_number--;
+                    count_2 += 1;
+                    if (new_number % 7 == 0)
+                    {
+                        Console.WriteLine(new_number);
+                    }
+                }
+
+
+            }
+
+
+        }
+    }
+}
