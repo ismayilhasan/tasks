434e93b6c1bdde560cb39c97fe3773ff21c076f7
ismayilhasan <ismayilrha@code.edu.az>
2022-04-22 18:05:10 +0400

    homework string method tasks

---
+++ homework_task_toCaptalized/homework_task_toCaptalized/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace homework_task_toCaptalized
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sAlam BURDA MeTHod oLMaliDir");
+            Console.WriteLine(toCaptalized("sAlam BURDA MeTHod oLMaliDir")); 
+
+
+        }
+
+
+        public static string toCaptalized(string sentence)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            sentence = sentence.ToLower();
+            string[] words = sentence.Split();
+
+            foreach(string word in words)
+            {
+                stringBuilder.Append(char.ToUpper(word[0]));
+                for (int i = 1; i < word.Length; i++)
+                {
+                    stringBuilder.Append(word[i]);
+                }
+                if (word != words[words.Length - 1])
+                {
+                    stringBuilder.Append(" ");
+                }
+                
+            }
+            return stringBuilder.ToString();
+
+        }
+    }
+}
