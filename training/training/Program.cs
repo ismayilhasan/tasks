fad88306d21be1ad948332d0318b900249443c4c
ismayilhasan <ismayilrha@code.edu.az>
2022-04-22 17:55:49 +0400

    Class task first

---
+++ training/training/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+namespace training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student = new Student("Ismayil","Hasanov",20,"Male",98,true);
+            Console.WriteLine(student.getInfo());
+
+            
+        }
+
+
+    }
+
+    class Student
+    {
+        public string Name;
+        public string Surname;
+        public byte Age;
+        public string Gender;
+        public byte Point;
+        public bool isGraduated;
+
+        public Student(string name,string surname,byte age,string gender,byte point,bool result )
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Gender = gender;
+            Point = point;
+            isGraduated = result;
+        }
+
+        public string getInfo()
+        {
+            
+            
+            
+                return $"Name : {Name} \nSurname : {Surname} \nAge {Age}   \nGender : {Gender} \nPoint : {Point} \ntelebe mezun olub";
+            
+           
+        }
+    }
+}
